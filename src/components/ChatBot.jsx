82bd6dd008c654890741739c5b72ce7fdf062155
TomWoolford <tom.woolford@emplas.co.uk>
2022-11-28 11:22:37 +0000

    Add content div to button

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types';
 import './chat.css';
 import ChatContainer from './ChatContainer';
+import ChatContent from './ChatContent';
 import ChatHeader from './ChatHeader';
 
 const ChatBot = () => {
     return (
         <ChatContainer>
             <ChatHeader />
+            <ChatContent />
         </ChatContainer>
     )
 }
---
+++ src/components/ChatContent.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+import './chat.css';
+
+const ChatContent = () => {
+    return (
+        <div className='content'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci sed magnam minima numquam, minus mollitia cumque? Libero perspiciatis repellendus cumque inventore iure, nemo provident repudiandae, ratione dolor quo error corporis?
+        </div>
+    )
+}
+
+ChatContent.propTypes = {
+
+};
+
+export default ChatContent;
