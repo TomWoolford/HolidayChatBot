dc912985a565419972a631a8f53287c9deff3f60
TomWoolford <tom.woolford@emplas.co.uk>
2022-11-28 10:06:29 +0000

    Add basic css and folder structure

---
+++ src/components/collapse/Collapseable.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import './collapse.css';
+
+const Collapseable = () => {
+    return (
+        <div className="container">
+            YO
+        </div>
+    )
+}
+
+Collapseable.propTypes = {
+
+}
+
+export default Collapseable;
+
