9ef86b2eb142e2fe3110c5a19d03c9a0c04a17a8
TomWoolford <tom.woolford@emplas.co.uk>
2022-11-28 12:33:20 +0000

    Add overflow to content

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import ChatBot from './components/ChatBot'
 
 function App() {
   return (
-    <div className="App row">
+    <div className="App">
       <div className="title row">
         <h1>Holiday Chat Protoype</h1>
       </div>
-      <div className="content">
+      <div className="main-content row">
         <ChatBot />
       </div>      
     </div>
