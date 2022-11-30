ca635730fcf8e725de7353e8de0e97c12f636041
TomWoolford <tom.woolford@emplas.co.uk>
2022-11-30 15:19:52 +0000

    Scroll to first element in result

--- src/components/responses/Responses.jsx
+++ src/components/responses/Responses.jsx
@@ -6,6 +6,7 @@ import './responses.css';
 const Responses = ({ messages }) => { 
     const hiddenRef = useRef(0);
     const resultStartRef = useRef(0);
+    const redundantRef = useRef(0);
     
     useEffect(() => {
         if (resultStartRef && resultStartRef.current)
@@ -15,7 +16,7 @@ const Responses = ({ messages }) => {
             return scroll(hiddenRef.current);
     }, [messages]);
 
-    const scroll = curr => curr.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    const scroll = curr => curr.scrollIntoView({ behavior: 'smooth', block: 'center' });
 
     // Will need to look into memo so it doesn't re-render everything 
     return (
@@ -25,6 +26,7 @@ const Responses = ({ messages }) => {
                     const cssClass = message.isUser ? `message user ${message.key}` : `message bot ${message.key}`;
                     return message.key !== "" ?
                         <p 
+                            ref={message.key === "matches" ? resultStartRef : redundantRef}
                             className={cssClass}
                             key={`messages-${idx}`} 
                             dangerouslySetInnerHTML={{__html: message.msg}} 
